4c94436f9dc1a09b21740aa507e12f96bcc2ebf9
Ilya Dobatovkin <ramon_mattlov@vulong-development.ru>
2024-12-06 02:31:19 +0300

    chore: added endpoints

--- main.py
+++ main.py
@@ -1,14 +1,59 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class NewPost(BaseModel):
+    title: str
+    author: str
+
+
+posts = [
+    {
+        'id': 1,
+        'title': 'Python lessons',
+        'author': 'Jack Jones'
+    },
+    {
+        'id': 2,
+        'title': 'Python practice',
+        'author': 'Mark Daniel'
+    }
+]
+
+
 @app.get('/', summary='root', tags=['Root routes'])
 def root():
-    return 'Hello world'
+    return 'success'
 
+
+@app.get('/posts', summary='Get posts', tags=['posts'])
+def get_posts():
+    return posts
+
+
+@app.get('/posts/{post_id}', summary='Get post by id', tags=['posts'])
+def get_post(post_id: int):
+    for post in posts:
+        if post['id'] == post_id:
+            return post
+    raise HTTPException(status_code=404, detail='Post not found')
+
+
+@app.post('/posts', summary='Create a new post', tags=['posts'])
+def create_post(new_post: NewPost):
+    posts.append({
+        'id': len(posts) + 1,
+        'title': new_post.title,
+        'author': new_post.author
+    })
+
+
+@app.delete('/posts', summary='Create a new post', tags=['posts'])
+def delete_post():
+    return
 
 if __name__ == '__main__':
     uvicorn.run('main:app', port=8080, reload=True)
-
